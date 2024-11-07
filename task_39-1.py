7823cbd4b92a8a9c209dfad23a786fd6bd6d8c8b
yourunb <bxyura@gmail.com>
2024-11-07 12:15:18 +0300

    feat: add task solution

--- task_39-1.py
+++ task_39-1.py
@@ -1,3 +1,18 @@
 str = input().lower()
 
-print(str)
+nums = []
+chars = []
+
+for char in str:
+    if char.isdigit():
+        nums.append(char)
+    else: 
+        if char != ' ':
+            chars.append(char)
+
+nums = sorted(list(set(nums)))
+chars = sorted(list(set(chars)))
+
+res = nums + chars
+
+print(' '.join(res))
