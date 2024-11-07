f54c9dcb51e70878dad3bdbde9cbc014d8b0bf6e
yourunb <bxyura@gmail.com>
2024-11-07 12:17:51 +0300

    feat: add solution 2 for task

--- task_39-1.py
+++ task_39-1.py
@@ -1,3 +1,4 @@
+#solution 1
 str = input().lower()
 
 nums = []
@@ -16,3 +17,10 @@ chars = sorted(list(set(chars)))
 res = nums + chars
 
 print(' '.join(res))
+
+#solution 2
+input_string = input().strip()
+cleaned_string = input_string.replace(' ', '').lower()
+unique_chars = set(cleaned_string)
+sorted_chars = sorted(unique_chars)
+print(" ".join(sorted_chars))
