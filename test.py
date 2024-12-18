26737b273f3b4a19f79a8d991e8db8572f7a1396
yourunb <bxyura@gmail.com>
2024-12-18 13:54:50 +0300

    refactor: update test python

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
-from datetime import datetime as dt
+from datetime import datetime
 
-print(dt.strptime("2023-31-12T23:59:59", "%Y-%d-%mT%H:%M:%S"))
+print(datetime.strptime("2023-31-12T23:59:59", "%Y-%d-%mT%H:%M:%S"))
+print(datetime.now())
+print(datetime.now().time())
